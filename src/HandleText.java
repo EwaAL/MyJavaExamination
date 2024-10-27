cdd02d147d9a2bd342bf4a405a0eb0b41f6d6621
EwaAL <ewa_almkvist@hotmail.com>
2024-10-28 00:43:02 +0100

    Har börjat skriva koden för testklassen, och lite i logik-klassen. Inte testkört ännu, det är för tidigt.

--- src/HandleText.java
+++ src/HandleText.java
@@ -1,2 +1,18 @@
 public class HandleText {
+
+    //Räkna raderna (ej raden STOP)
+    // Räkna antal tecken (ej ordet STOP)
+    // Räkna antal ord (ej ordet STOP)
+    //Ha koll på det längsta ordet (ej ordet STOP)
+    //Har koll på om användaren har skrivit ordet stop eller inte
+
+    private String text;
+    private int rows;
+    private int chars;
+    private int words;
+    private String longestWord;
+
+    public HandleText(String inputString) {
+        text=inputString;
+    }
 }
--- src/HandleTextTest.java
+++ src/HandleTextTest.java
@@ -1,3 +1,48 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class HandleTextTest {
-    
+    //Skriv ett program som läser in text ifrån kommandoraden rad för rad
+    // tills användaren skriver ordet stop.
+    //När användaren är klar skriver programmet ut antal tecken
+    // och hur många rader som användaren har skrivit,
+    // exklusive raden med ordet stop.
+
+    //Till programmet skall minst tre Junit-testfall skrivas
+    //Testfallen skall skilja sig märkvärt åt och testa olika delar av programmet.
+    //Programmet skall versionshanteras och finnas tillgängligt på Github.
+
+    @Test
+    public void testCharCount(){
+
+        String inputString = "Jag testar att skriva en massa ord stop";
+        HandleText myTextHandler = new HandleText(inputString);
+
+        assertEquals(35,myTextHandler.charCount);
+    }
+    public void testRowCount(){
+
+        int rows;
+        String inputString = "Jag testar att skriva en massa ord stop";
+        HandleText myTextHandler = new HandleText(inputString);
+
+        assertEquals(35,myTextHandler.charCount);
+
+    }
+    public void testLongestWord(){
+
+        assertEquals(true, blnOK);
+    }
+    public void testWordsTotal(){
+
+        assertEquals(true, blnOK);
+    }
+    public void testStopWord(){
+
+        assertEquals(true, blnOK);
+    }
+
+
+
+
 }

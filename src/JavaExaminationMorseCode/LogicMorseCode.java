ba338a33e400a2f9b7c015ac23178eb46ef834a6
EwaAL <ewa_almkvist@hotmail.com>
2025-01-31 11:44:34 +0100

    Har löst strulet med logik-klassen, det berodde på en måsvinge som hamnat fel...

--- src/JavaExaminationMorseCode/LogicMorseCode.java
+++ src/JavaExaminationMorseCode/LogicMorseCode.java
@@ -1,49 +1,43 @@
+
 package JavaExaminationMorseCode;
 
 import java.util.Scanner;
 
 public class LogicMorseCode {
-    package JavaExaminationMorseCode;
+    //Skriv ett program som gör om morsekod till engelska
+    //och engelska till morsekod
+    //❖ Skapa minst två felhanteringar i systemet som
+    //hanterar två saker som användaren kan göra som går
+    //utanför det vanliga användandet av systemet
+    //Använd den internationella morsekoden (se slide 6)
+    //för bokstäverna A till Z
+    //❖ Korta och långa signaler ska representeras med
+    //punkt (.) och bindestreck (-)
+    //❖ F = ..-.
+    //❖ Flera bokstäver skall gå att hantera men separation
+    //av ord behöver inte hanteras
+    //❖ …. . .--- = HEJ
+    //❖ .... . .-.. .-.. --- .-- --- .-. .-.. -.. = HELLOWORLD
+    //❖ HELLO WORLD = .... . .-.. .-.. --- .-- --- .-. .-.. -..
+    private String[] textToUse;
 
-import java.io.*;
-import java.util.Scanner;
-
-    public class LogicMorseCode {
-        //Skriv ett program som gör om morsekod till engelska
-        //och engelska till morsekod
-        //❖ Skapa minst två felhanteringar i systemet som
-        //hanterar två saker som användaren kan göra som går
-        //utanför det vanliga användandet av systemet
-        //Använd den internationella morsekoden (se slide 6)
-        //för bokstäverna A till Z
-        //❖ Korta och långa signaler ska representeras med
-        //punkt (.) och bindestreck (-)
-        //❖ F = ..-.
-        //❖ Flera bokstäver skall gå att hantera men separation
-        //av ord behöver inte hanteras
-        //❖ …. . .--- = HEJ
-        //❖ .... . .-.. .-.. --- .-- --- .-. .-.. -.. = HELLOWORLD
-        //❖ HELLO WORLD = .... . .-.. .-.. --- .-- --- .-. .-.. -..
-        private String[] textToUse;
-
-        public boolean printOut(String text) {
-            System.out.println(text);
-            return true;
-
-        }
-
-        public String readText() {
-
-            String text = "";
-
-            Scanner myScan = new Scanner(System.in);
-            System.out.println("Write something in English or in Morse Code");
-            text = myScan.nextLine();
-
-            return text;
-        }
+    public boolean printOut(String text) {
+        System.out.println(text);
+        return true;
 
     }
+
+    public String readText() {
+
+        String text = "";
+
+        Scanner myScan = new Scanner(System.in);
+        System.out.println("Write something in English or in Morse Code");
+        text = myScan.nextLine();
+
+        return text;
+    }
+
 
     public void splitText() {
 
@@ -59,4 +53,3 @@ import java.util.Scanner;
 
 }
 
-}
